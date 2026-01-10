26d3bb569ca87707dd007ea2e80cffc772e05f63
RohyllerP <rohyler@gmail.com>
2026-01-10 17:19:18 +0000

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -6,12 +6,5 @@ export default defineNuxtConfig({
       }
     }
   },
-  runtimeConfig: {
-    apiSecretKey: process.env.FULL_API_TOKEN,
-    public: {
-      APP_MODE: process.env.NUXT_APP_MODE,
-      apiSecretKey: process.env.FULL_API_TOKEN
-    }
-  },
   css: ['~/assets/css/main.css'],
 })
